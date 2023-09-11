11ec837cfb0bf857a5f4d339c93265774bbe7047
Nadifslebew <144640443+Nadifslebew@users.noreply.github.com>
2023-09-11 09:47:23 +0700

    Create Luas_persegi.cpp

---
+++ Luas_persegi.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int S, L;
+    cout<<"MENGHITUNG LUAS PERSEGI \n";
+    cout<<"<=====================> \n";
+    cout<<"masukan angka:";
+    cin>>S;
+    L=S*S;
+    cout<<"hasil adalah:"<<L<<endl;
+    return 0;}
